436d756f470795cd001765e445695faa29c4e19d
Aditya Kumar <aditya147012@gmail.com>
2024-04-05 15:27:42 +0530

    Adding all files

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+    const sideBar = document.querySelector('.sideBar'); // corrected selector
+    sideBar.style.display = "flex";
+  }
+  
+  function hideSideBar() {
+    const sideBar = document.querySelector('.sideBar'); // corrected selector
+    sideBar.style.display = "none";
+  }
